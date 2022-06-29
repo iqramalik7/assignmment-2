048aedf8ffc229cde2ab1abf340e2339b3e084e1
Abdullah <Abdullah@WIN-84BVOTR7HBP>
2022-06-29 06:50:29 +0500

    Add project files.

---
+++ web assignment2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult contact()
+        {
+            return View();
+        }
+    }
+}
