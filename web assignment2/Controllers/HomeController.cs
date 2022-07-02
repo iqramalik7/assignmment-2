bed812a0a790c7f45efe9f06485b25252f3b3e3f
Iqra Malik <Iqra Malik@DESKTOP-EF3D8H9>
2022-07-02 14:03:21 +0500

    Quiz 2

--- web assignment2/Controllers/HomeController.cs
+++ web assignment2/Controllers/HomeController.cs
@@ -11,7 +11,21 @@ namespace web_assignment2.Controllers
 
         public IActionResult contact()
         {
+            ViewBag.message = "LOGIN";
             return View();
+        }
+
+        public IActionResult formm(string fname, string lname, string email, double phoneno, string gender, string dep, string textbox)
+        {
+            if(fname != null && lname != null && email != null && phoneno != null && gender != null && dep != null && textbox != null)
+            {
+                ViewBag.message = "Successfull";
+            }
+            else
+            {
+                ViewBag.message = "Failed";
+            }
+            return View("contact");
         }
     }
 }
